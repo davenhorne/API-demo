e0fcca88ba83c9968ccfbfd1557d0d5aae997a83
Daven <davenhorne31@gmail.com>
2022-04-01 17:50:59 -0400

    API demo

---
+++ controller.js
@@ -0,0 +1,50 @@
+window.onload = () => {
+    document.querySelector("#submit").addEventListener("click", getNewPokemon);
+
+}
+
+function getNewPokemon(){
+    /*
+    1.Call to API _DONE_
+    2.Wait for API to respond _DONE_
+    3. Get response data _DONE_
+    4. Put data in HTML
+    */
+ let pokemonName = document.querySelector("#pokemon-input").value;
+
+    getPokemon(pokemonName).then(resultJson =>{
+        populateHTML(resultJson);
+    });
+
+}
+
+async function getPokemon(pokemonName){
+    let endpoint =" https://pokeapi.co/api/v2/pokemon/"+pokemonName;
+    let req = new Request(endpoint);
+    let response = await fetch(req);
+    return response.json();
+}
+
+function populateHTML(pkmJson) {
+    let header = document.querySelector("#pokemon-name");
+    let img = document.querySelector("#pokemon-sprite");
+
+     header.innerHTML = getpokemonName(pkmJson);
+     img.setAttribute("src", getPokemonSprite(pkmJson));
+
+    }
+
+
+function getpokemonName(pkmJson){
+ return capitalize(pkmJson.name);
+}
+
+function getPokemonSprite(pkmJson){
+ return pkmJson.sprites.front_shiny;
+}
+
+function capitalize(string) {
+ let start = string.substring(0,1).toUpperCase();
+ let body = string.substring(1).toLowerCase();
+ return start+body;
+}
